ea0bb5c406f7edfd18eb4de50bc5e2c8b15cd727
Elliot Marx <elliot@chalk.ai>
2022-09-17 18:56:01 -0700

    Install Chalk CLI

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# chalk-actions
+# `cli-action`
+> Sets up the Chalk CLI for tests in GitHub
+
+## About
+This action sets up the Chalk CLI, `chalk` on GitHub's hosted Actions runners.
+
+This action can be run on `ubuntu-latest`, `windows-latest`, and `macos-latest` 
+GitHub Actions runners, and will install and expose a specified version of the 
+`chalk` CLI on the runner environment.
+
+## Usage
+
+Set up the `chalk` CLI:
+
+```yaml
+steps:
+- uses: chalk-ai/cli-action@v1
+  with:
+    client-id: ...
+    client-secret: ...
+    environment: ...
+```
+
+A specific version of the `chalk` CLI can be installed:
+
+```yaml
+steps:
+- uses: chalk-ai/cli-action@v1
+  with:
+    version: 1.1.0
+    client-id: ...
+    client-secret: ...
+```
+
+## Inputs
+The action supports the following inputs:
+
+- `client-id`: The Chalk Client ID from the tokens page in your settings.
+- `client-secret`: The Chalk Client Secret from the tokens page in your settings.
+- `environment` (optional): The Chalk environment to use.
+- `version` (optional): The version of `chalk` to install, defaulting to `latest`
+- `api-host` (optional): If you're using a self-hosted deployment, the API host where Chalk is hosted.
+
+## License
+Apache 2.0
---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import * as core from '@actions/core'
+import * as http from '@actions/http-client'
+import * as toolCache from '@actions/tool-cache'
+import * as fs from "fs";
+
+const client = new http.HttpClient('setup-chalk-cli')
+const TOOL_NAME = 'chalk'
+
+async function run() {
+  // Get user-specified version to install (defaults to "latest")
+  const suppliedVersion = core.getInput('version')
+  const os = process.platform
+  const arch = process.arch
+  const version = suppliedVersion == "latest" ? await getLatestVersion(os, arch) : suppliedVersion
+
+  const cachedPath = toolCache.find(TOOL_NAME, version)
+  if (cachedPath && version != "nightly") {
+    core.addPath(cachedPath)
+  } else {
+    core.info(`Downloading version ${version}`)
+    const path = await toolCache.downloadTool(`https://api.prod.chalk.ai/v1/install/${os}/${arch}/${version}`)
+    fs.chmodSync(path, 0o755)
+    const cachedPath = await toolCache.cacheFile(path, TOOL_NAME, TOOL_NAME, version, undefined)
+    fs.chmodSync(cachedPath, 0o755)
+    core.addPath(cachedPath)
+  }
+
+  fs.writeFileSync(
+      '/home/runner/.config/.chalk.yml',
+`tokens:
+  default:
+     name: Default Token
+     clientId: ${core.getInput("client-id")}
+     clientSecret: ${core.getInput("client-secret")}
+     apiServer: ${core.getInput('api-host') || "https://api.prod.chalk.ai"}
+     activeEnvironment: ${core.getInput("environment")}
+`, "utf-8")
+
+  core.info(`${TOOL_NAME} is installed`)
+}
+
+async function getLatestVersion(os: string, arch: string) {
+  core.info(`Checking latest version...`)
+  const versionResponse = await client.get(`https://api.prod.chalk.ai/v2/install/versions/${os}/${arch}`)
+  const body = await versionResponse.readBody()
+  return JSON.parse(body)["latest_version"]
+}
+
+run().catch((error) => {
+  if (error instanceof Error) {
+    core.setFailed(error.message)
+  } else {
+    core.setFailed(`${error}`)
+  }
+})
